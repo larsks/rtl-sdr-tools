2d48a8660167e5bc7229a9a2bbd3ac1ffb1999d4
Lars Kellogg-Stedman <lars@oddbit.com>
2019-09-04 12:54:13 -0400

    add flex decoder

--- decode-gqrx-pocsag.sh
+++ decode-gqrx-pocsag.sh
@@ -2,6 +2,8 @@
 
 nc -l -u 7355  |
 	sox \
-		-r 48000 -t raw -b 16 -c 1 -e signed-integer /dev/stdin \
+		-r 48000 -t raw -b 16 -c 1 -e signed-integer - \
 		-r 22050 -t raw -b 16 -c 1 -e signed-integer - |
-	multimon-ng -t raw -c -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -f alpha -
+	multimon-ng -t raw -c \
+		-a FLEX -a POCSAG512 -a POCSAG1200 -a POCSAG2400 \
+		-f alpha -
